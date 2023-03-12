9190e3a0b2abe964df219e4ef5f882b3e4b58449
daoraCode <severin.mboukou@gmail.com>
2023-03-12 23:43:50 +0100

    react-router-dom dependency installed, working on contexts for restaurant values

---
+++ src/contexts/RestaurantsContext.tsx
@@ -0,0 +1,43 @@
+import React, { createContext, useContext, useState } from 'react';
+import { Restaurant } from '../models/Restaurant';
+
+type RestaurantsContextProps = {
+  children: React.ReactNode;
+};
+
+// array of restaurants
+export type RestaurantsContextType = {
+  restaurants: Restaurant[];
+};
+
+// createContext hook invoked for using restaurant values from anywhere
+export const RestaurantsContext = createContext<RestaurantsContextType | null>(
+  null
+);
+
+// custom useContext() hook,
+// runtime checking when provider not wrapping component properly
+// (other way could be type assertion)
+export const useRestaurant = () => {
+  const restaurantsContext = useContext(RestaurantsContext);
+
+  if (!restaurantsContext) {
+    throw new Error(
+      'resurantContext has to be used within <RestaurantContext.Provider>'
+    );
+  }
+
+  return restaurantsContext;
+};
+
+export const RestaurantsContextProvider = ({
+  children,
+}: RestaurantsContextProps) => {
+  const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
+
+  return (
+    <RestaurantsContext.Provider value={{ restaurants }}>
+      {children}
+    </RestaurantsContext.Provider>
+  );
+};
